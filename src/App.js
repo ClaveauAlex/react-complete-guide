a0f794c3048fae27aa79363f837b07efdc9e8acb
Alexandre Claveau <a.claveau29@gmail.com>
2021-02-14 19:25:08 +0100

    Creating a functional component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import './App.css';
+import Person from "./Person/Person";
 
 function App() {
   return (
     <div className="App">
       <h1>Hey I'm a React App</h1>
+      <p>This is working</p>
+      <Person />
     </div>
   );
 }
