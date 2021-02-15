7c6226dc6427148a9b3bfd8e4dff8ac1bc577604
Alexandre Claveau <a.claveau29@gmail.com>
2021-02-15 17:19:50 +0100

    using the useState() Hook for state manipulation

--- src/App.js
+++ src/App.js
@@ -1,51 +1,71 @@
-import { Component } from "react";
+import React, { useState } from "react";
 import "./App.css";
 import Person from "./Person/Person";
 
-class App extends Component {
-  state = {
+function App(props) {
+  const [personsState, setPersonsState] = useState({
     persons: [
       { name: "Max", age: 28 },
       { name: "Manu", age: 29 },
-      { name: "Stephanie", age: 26 },
+      { name: "Stephanie", age: 26 }
     ],
-  };
+    otherState: "some other value"
+  });
 
-  switchNameHandler = () => {
+  const switchNameHandler = () => {
     // console.log("Was cliked !");
     // Don't do this ! this.state.persons[0].name = "Maximilian";
-    this.setState({
+    setPersonsState({
       persons: [
         { name: "Maximilian", age: 28 },
         { name: "Manu", age: 29 },
         { name: "Stephanie", age: 27 },
       ],
+      otherState: personsState.otherState
     });
   };
 
-  render() {
-    return (
-      <div className="App">
-        <h1>Hey I'm a React App</h1>
-        <p>This is working</p>
-        <button onClick={this.switchNameHandler}>Switch Name</button>
-        <Person
-          name={this.state.persons[0].name}
-          age={this.state.persons[0].age}
-        />
-        <Person
-          name={this.state.persons[1].name}
-          age={this.state.persons[1].age}
-        >
-          My hobbies: Racing
-        </Person>
-        <Person
-          name={this.state.persons[2].name}
-          age={this.state.persons[2].age}
-        />
-      </div>
-    );
-  }
-}
+  return (
+    <div className="App">
+      <h1>Hey I'm a React App</h1>
+      <p>This is working</p>
+      <button onClick={switchNameHandler}>Switch Name</button>
+      <Person
+        name={personsState.persons[0].name}
+        age={personsState.persons[0].age}
+      />
+      <Person
+        name={personsState.persons[1].name}
+        age={personsState.persons[1].age}
+      >
+        My hobbies: Racing
+      </Person>
+      <Person
+        name={personsState.persons[2].name}
+        age={personsState.persons[2].age}
+      />
+    </div>
+  );
+};
 
 export default App;
+
+// state = {
+//   persons: [
+//     { name: "Max", age: 28 },
+//     { name: "Manu", age: 29 },
+//     { name: "Stephanie", age: 26 },
+//   ],
+// };
+
+// switchNameHandler = () => {
+//   // console.log("Was cliked !");
+//   // Don't do this ! this.state.persons[0].name = "Maximilian";
+//   this.setState({
+//     persons: [
+//       { name: "Maximilian", age: 28 },
+//       { name: "Manu", age: 29 },
+//       { name: "Stephanie", age: 27 },
+//     ],
+//   });
+// };
